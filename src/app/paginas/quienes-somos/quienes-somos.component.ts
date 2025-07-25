b0968430387589594ce74133d1f54b4d73a4ec98
Random20244 <enivan.1689@gmail.com>
2025-07-25 10:54:23 -0300

    nashe2

--- src/app/paginas/quienes-somos/quienes-somos.component.ts
+++ src/app/paginas/quienes-somos/quienes-somos.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { equipo } from '../../modelos/quienes-somos.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { equipo } from '../../modelos/quienes-somos.model';
 
 @Component({
   selector: 'app-quienes-somos',
@@ -15,22 +15,26 @@ export class QuienesSomosComponent {
     {
       nombre: 'Benjamín Pino',
       rol: 'Fundador de la pagina web y crador del negocio',
-      foto: ''
+      foto: 'https://img.freepik.com/foto-gratis/hombre-pulgar-arriba_1149-1712.jpg?semt=ais_hybrid&w=740'
     },
     {
       nombre: 'William Barnegas',
       rol: 'Mano derecha del fundador',
-      foto: ''
+      foto: 'https://img.freepik.com/foto-gratis/hombre-sonriendo-pulgar-arriba_1139-678.jpg'
     },
     {
       nombre: 'Barbara Salvo',
       rol: 'Madre del fundador y apoyadora financiera',
-      foto: ''
+      foto: 'https://static8.depositphotos.com/1010146/915/i/450/depositphotos_9153000-stock-photo-joyful-young-woman-showing-ok.jpg'
     },
     {
       nombre: 'Enoc Ivan Pino',
       rol: 'Padre y apoyo financiero',
-      foto: ''
+      foto: 'https://e1.pngegg.com/pngimages/772/98/png-clipart-willy-alex-man-showing-double-thumbs-up.png'
+    },
+    {nombre: 'Lisandro Garcia',
+      rol: 'Mano izquierda del fundador',
+      foto: 'https://img.freepik.com/fotos-premium/personaje-blanco-3d-empresario-pulgar-arriba_168450-116.jpg'
     }
   ];
 }
